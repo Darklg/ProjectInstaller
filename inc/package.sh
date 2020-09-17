0696bd2689bd0d996486a3f1636561938bf93038
Kevin <kevin@rocher-dev.com>
2020-09-17 20:10:47 +0200

    v 0.4.1
    Better project packaging for Magento

--- inc/package.sh
+++ inc/package.sh
@@ -17,8 +17,12 @@ fi;
 cd "${BASEDIR}/${_INSTALL_FOLDER}";
 
 # Delete useless folders
-rm -rf .git vendor var wp-content/cache wp-content/uploads;
+rm -rf .git wp-content/cache wp-content/uploads;
 find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
+
+if [[ -f "bin/magento" ]];then
+    rm -rf pub generated var vendor;
+fi;
 
 cd "${BASEDIR}";
 
@@ -30,6 +34,10 @@ zip -r "htdocs-${_DATE_STAMP}.zip" "${_INSTALL_FOLDER}"
 ###################################
 
 cd "${BASEDIR}";
-rm deploy.sh wputools-local.sh wp-cli.phar wputools-local.sh wputools-urls.txt *.sublime-project *.sublime-workspace;
+rm wputools-local.sh wp-cli.phar wputools-local.sh wputools-urls.txt *.sublime-workspace;
+
+if [[ ! -f "bin/magento" ]];then
+    rm -rf deploy.sh *.sublime-project;
+fi;
 rm -rf logs;
 rm -rf "${BASEDIR}/${_INSTALL_FOLDER}";
