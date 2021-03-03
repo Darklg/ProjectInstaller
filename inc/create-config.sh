1d4f29422ef0d1b75a07cf9316100e17bfff588d
Kevin <kevin@rocher-dev.com>
2021-03-03 11:59:15 +0100

    v 0.8.0
    Creation :
    - Auto-fill repository & WordPress MySQL variables.

--- inc/create-config.sh
+++ inc/create-config.sh
@@ -25,3 +25,35 @@ _use_project_dirname=$(bashutilities_get_yn "- Is this project named '${_PROJECT
 if [[ "${_use_project_dirname}" == 'y' ]];then
     bashutilities_sed "s/myproject/${_PROJECT_DIRNAME}/g" "${_INSTALL_FILE}";
 fi;
+
+# Repository
+if [[ -f "${BASEDIR}htdocs/.git/config" ]];then
+    _PROJECT_REPO=$(cd "${BASEDIR}htdocs/" && git config --get remote.origin.url);
+    bashutilities_sed "s#mygitrepository#${_PROJECT_REPO}#g" "${_INSTALL_FILE}";
+fi;
+
+# MySQL
+_config_file="${BASEDIR}htdocs/wp-config.php";
+if [[ -f "${_config_file}" ]];then
+    _TMP_DB_NAME=$(bashutilities_search_extract_file__php_constant "DB_NAME" "${_config_file}");
+    _TMP_DB_USER=$(bashutilities_search_extract_file__php_constant "DB_USER" "${_config_file}");
+    _TMP_DB_PASSWORD=$(bashutilities_search_extract_file__php_constant "DB_PASSWORD" "${_config_file}");
+    _TMP_DB_HOST=$(bashutilities_search_extract_file__php_constant "DB_HOST" "${_config_file}");
+    _TMP_DB_PREFIX=$(bashutilities_search_extract_file "\$table_prefix =" "';" "${_config_file}");
+    _TMP_DB_PREFIX=${_TMP_DB_PREFIX/\'/};
+    if [[ -n "${_TMP_DB_NAME}" && -n "${_TMP_DB_USER}" && -n "${_TMP_DB_PASSWORD}" && -n "${_TMP_DB_HOST}" && -n "${_TMP_DB_PREFIX}" ]];then
+        echo "NAME : ${_TMP_DB_NAME}";
+        echo "USER : ${_TMP_DB_USER}";
+        echo "PASSWORD : ${_TMP_DB_PASSWORD}";
+        echo "HOST : ${_TMP_DB_HOST}";
+        echo "PREFIX : ${_TMP_DB_PREFIX}";
+        _use_tmp_values=$(bashutilities_get_yn "- Use MySQL values?" 'y');
+        if [[ "${_use_tmp_values}" == 'y' ]];then
+            bashutilities_sed "s#mysqlbase#${_TMP_DB_NAME}#g" "${_INSTALL_FILE}";
+            bashutilities_sed "s#mysqluser#${_TMP_DB_USER}#g" "${_INSTALL_FILE}";
+            bashutilities_sed "s#mysqlpass#${_TMP_DB_PASSWORD}#g" "${_INSTALL_FILE}";
+            bashutilities_sed "s#mysqlhost#${_TMP_DB_HOST}#g" "${_INSTALL_FILE}";
+            bashutilities_sed "s#mysqlpref_#${_TMP_DB_PREFIX}#g" "${_INSTALL_FILE}";
+        fi;
+    fi;
+fi;
--- tpl/base-config-file.sh
+++ tpl/base-config-file.sh
@@ -5,18 +5,18 @@
 ###################################
 
 # Database
-_MYSQL_USER='root';
-_MYSQL_PASS='root';
-_MYSQL_HOST='localhost';
-_MYSQL_BASE='mydb';
-_MYSQL_PREFIX='pref_';
+_MYSQL_USER='mysqluser';
+_MYSQL_PASS='mysqlpass';
+_MYSQL_HOST='mysqlhost';
+_MYSQL_BASE='mysqlbase';
+_MYSQL_PREFIX='mysqlpref_';
 
 # Project
 _PROJECT_ID='myproject';
 _PROJECT_DOMAIN='myproject.test';
 _PROJECT_HTTP='http';
 _PROJECT_NAME='myproject';
-_PROJECT_REPO='git@gitlab.com:myproject/myproject.git';
+_PROJECT_REPO='mygitrepository';
 
 # Install
 _INSTALL_CMS='wordpress';
